6324820983aabda115463377e0e2909f4351df84
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-06-26 11:29:35 -0400

    aer

--- app/actions/GetArticlesByCategory.ts
+++ app/actions/GetArticlesByCategory.ts
@@ -1,26 +1,23 @@
 
 
 import { Article } from "@/interfaces/article.interface";
+import prismadb from "@/lib/prismadb";
 
 export async function getArticlesByCategory(categoria: string): Promise<Article[] | []>{
   console.log("entro aca al action createReport", categoria);
 
   try {
-    const res = await fetch(
-      `${process.env.NEXTAUTH_URL}/api/getArticle?categoria=${categoria}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        cache: 'no-store'
-      }
-    );
-
-    const articles = await res.json();
+    console.log("entro", categoria);
+    const articles = await prismadb.articulo.findMany({
+      where: {
+        categoria: categoria,
+      },
+    });
+  
     console.log(articles);
-
+  
     return articles
+
   } catch (error) {
     console.log(error);
 
