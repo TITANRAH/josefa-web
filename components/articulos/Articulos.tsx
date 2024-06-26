15f8426ad32ed3933414f3d57345a0dce0c7adf9
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-06-26 04:56:45 -0400

    ahoea si

--- components/articulos/Articulos.tsx
+++ components/articulos/Articulos.tsx
@@ -20,7 +20,7 @@ export default async function Articulos(props: Props) {
     <section className="flex flex-col">
       <h1 className="text-3xl text-orange-400 underline font-bold">{title}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-4 md:px-6 py-12 md:py-24">
-        {articulos != undefined && articulos.data.length > 0 ? (
+        {articulos && articulos != undefined && articulos.data.length > 0 ? (
           articulos.data.map((a: Article, index: any) => (
             <Articulo
               key={index}
